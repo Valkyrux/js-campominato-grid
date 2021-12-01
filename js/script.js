23e529420562a8ed7d866d9c19cc0a3e08435478
Valkyrux <philadephos.granata@gmail.com>
2021-12-01 15:44:08 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// genero il titolo della pagina
+const pageHeader = document.querySelector("header");
+const pageTitle = document.createElement("h1");
+pageTitle.append("Campo Minato: La Griglia");
