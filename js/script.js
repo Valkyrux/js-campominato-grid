b29d490d02ee6104334d21888e4379b3eed5e992
Valkyrux <philadephos.granata@gmail.com>
2021-12-01 17:54:20 +0100

    add-grid

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// funzione che genera la partita
+function playGame(choice, option1, option2, option3) {
+    let ciro;
+    if(choice.value == option1.value){
+        innerHTML = "";
+    } else if(choice.value == option2.value){
+        innerHTML = "";
+    } else if(choice.value == option3.value){
+        innerHTML = "";
+    }
+    return console.log(ciro);
+}
+
+
 // seleziono l'header
 const pageHeader = document.querySelector("header");
 // genero il titolo e lo metto in pagina
@@ -5,7 +19,7 @@ const pageTitle = document.createElement("h1");
 pageTitle.append("Campo Minato: La Griglia");
 pageHeader.firstElementChild.append(pageTitle);
 // genero la parte dell'header per selezionare la difficoltà
-const difficultyForm = document.createElement("form");
+const difficultyDiv = document.createElement("div");
 
 const difficultyLabel = document.createElement("label");
 difficultyLabel.append("difficoltà:");
@@ -14,28 +28,29 @@ difficultyLabel.for = "difficulty";
 const difficultySelect= document.createElement("select");
 difficultySelect.name = "difficulty"; 
 difficultySelect.id = "difficulty"; 
-difficultySelect.value = "easy"; 
 
-let difficultyOption = document.createElement("option");
-difficultyOption.innerHTML = "easy";
-difficultyOption.value = "easy";
-difficultySelect.appendChild(difficultyOption);
-difficultyOption = document.createElement("option");
-difficultyOption.innerHTML = "hard";
-difficultyOption.value = "hard";
-difficultySelect.appendChild(difficultyOption);
-difficultyOption = document.createElement("option");
-difficultyOption.innerHTML = "crazy";
-difficultyOption.value = "crazy";
-difficultySelect.appendChild(difficultyOption);
+const difficultyOption1 = document.createElement("option");
+difficultyOption1.innerHTML = "easy";
+difficultyOption1.value = "easy";
+difficultySelect.appendChild(difficultyOption1);
+const difficultyOption2 = document.createElement("option");
+difficultyOption2.innerHTML = "hard";
+difficultyOption2.value = "hard";
+difficultySelect.appendChild(difficultyOption2);
+const difficultyOption3 = document.createElement("option");
+difficultyOption3.innerHTML = "crazy";
+difficultyOption3.value = "crazy";
+difficultySelect.appendChild(difficultyOption3);
 
 const difficultyButton = document.createElement("button");
 difficultyButton.append("Play");
 
-difficultyForm.appendChild(difficultyLabel);
-difficultyForm.appendChild(difficultySelect);
-difficultyForm.appendChild(difficultyButton);
+difficultyDiv.appendChild(difficultyLabel);
+difficultyDiv.appendChild(difficultySelect);
+difficultyDiv.appendChild(difficultyButton);
 
-pageHeader.appendChild(difficultyForm);
+pageHeader.appendChild(difficultyDiv);
 
-difficultyButton.addEventListener("click")
+
+
+difficultyButton.addEventListener('click', () => {playGame(difficultySelect, difficultyOption1, difficultyOption2, difficultyOption3)});
